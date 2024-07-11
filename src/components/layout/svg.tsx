211faaa59e70d125e315a320eda513452cc43a09
Amal C P <acp070146@gmail.com>
2024-07-11 17:37:36 +0530

    clearn console errors

--- src/components/layout/svg.tsx
+++ src/components/layout/svg.tsx
@@ -24,9 +24,9 @@ export const Diamond = (_props: Props) => {
           width="26.0045"
           height="26"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
